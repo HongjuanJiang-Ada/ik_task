bcfc0aed4d165accca05d9e76827260b7baee449
 HongjuanJiang-Ada <jianghongjuan1996@gmail.com>
2024-04-28 16:58:10 +0200

    Create a CMake project for the task

---
+++ build/CMakeFiles/RobotArm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RobotArm.dir/task.cpp.o"
+  "CMakeFiles/RobotArm.dir/task.cpp.o.d"
+  "RobotArm"
+  "RobotArm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RobotArm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
